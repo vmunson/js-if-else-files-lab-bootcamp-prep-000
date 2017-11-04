b62810255179dcb746bdf0c22751dd937f77041b
Ginny Munson <virginia.munson@gmail.com>
2017-11-04 23:00:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var name = "Joe"
 var height = "74"
+
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
@@ -8,5 +9,3 @@ if (name === "Susan") {
 // Don't worry about this
 module.exports = { name, height
 }
-
-
